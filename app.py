549649e4dcf9273ec26b584604599609f7d0ba69
Mounith Varma Akkala <1mvakkala@gmail.com>
2026-08-18 11:42:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,3 @@ add_routes(
     formatted_agent_chain,
     path="/agent"
 )
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
